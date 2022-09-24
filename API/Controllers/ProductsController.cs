130dc80c2e4d09af4a039719b7e091d16ec8e837
FravinCustodianStudent <91590998+FravinCustodianStudent@users.noreply.github.com>
2022-09-24 18:54:38 +0300

    Initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+        private readonly StoreContext _context;
+
+        public ProductsController(ILogger<ProductsController> logger,StoreContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>>  GetProducts(){
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product !=null){
+                return Ok(product);
+            }else{
+                return NotFound();
+            }
+            
+        }
+    }
+}
