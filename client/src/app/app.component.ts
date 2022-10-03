4d86f8ddf9007852db1ef3d5324580e5550307e3
FravinCustodianStudent <91590998+FravinCustodianStudent@users.noreply.github.com>
2022-10-03 19:35:18 +0300

    Client base

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {IProduct} from "./models/product";
+import {IPagination} from "./models/pagination";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private http:HttpClient) {
+  }
   title = 'AngularShop';
+  products: IProduct[];
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe((response: IPagination)=>
+    {
+      this.products = response.data;
+    },err=>{
+      console.log(err)
+    });
+  }
 }
