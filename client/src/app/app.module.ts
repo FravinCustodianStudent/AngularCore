e50d7b7d2d62a08a053cbe7346ac9098ec76ddf0
FravinCustodianStudent <91590998+FravinCustodianStudent@users.noreply.github.com>
2022-10-06 17:46:18 +0300

    Error handlers

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,15 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {CoreModule} from "./core/core.module";
 import {ShopModule} from "./shop/shop.module";
 import {HomeModule} from "./home/home.module";
+import {ErrorInterceptor} from "./core/interceptors/error.interceptor";
+import {ToastrModule} from "ngx-toastr";
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -21,7 +24,9 @@ import {HomeModule} from "./home/home.module";
     CoreModule,
     HomeModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,useClass:ErrorInterceptor,multi:true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
