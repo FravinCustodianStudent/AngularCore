9cd02b71be1113edf51f5c1f2195f1bd252bdfc5
FravinCustodianStudent <91590998+FravinCustodianStudent@users.noreply.github.com>
2022-10-07 18:15:40 +0300

    Front Update v2

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,32 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {CoreModule} from "./core/core.module";
-import {ShopModule} from "./shop/shop.module";
 import {HomeModule} from "./home/home.module";
 import {ErrorInterceptor} from "./core/interceptors/error.interceptor";
-import {ToastrModule} from "ngx-toastr";
+import { NgxSpinnerModule} from "ngx-spinner";
+import {LoadingInterceptor} from "./core/interceptors/loading.interceptor";
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    HomeModule
+    HomeModule,
+    NgxSpinnerModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,useClass:ErrorInterceptor,multi:true
-  }],
+  },
+    {
+      provide: HTTP_INTERCEPTORS,useClass:LoadingInterceptor,multi:true
+    },
+  ],
+  exports:[NgxSpinnerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {
